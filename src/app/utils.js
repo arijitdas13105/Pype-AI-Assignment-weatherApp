0c51480e3788ca3928ba4f814708a8d253b58e14
arijitdas13105 <arijitdas13105@gmail.com>
2024-02-18 13:13:24 +0530

    updating

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,7 +1,1 @@
 export  const API_KEY="502d09b33ddd90d43bf476323d9e8481"
-
-export const Thermometer=[
-    { identifier: "c", name: "celcius" },
-    { identifier: "f", name: "Fahrenheit" },
-    { identifier: "k", name: "kelvin" },
-]
